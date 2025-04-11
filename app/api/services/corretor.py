e4cc5c8606e5fb99f2cfa4946ec52fffef52b370
LazimR <lrodrigues356@gmail.com>
2025-04-11 17:33:35 -0300

    Sistema de geração de provas e gabaritos

---
+++ app/api/services/corretor.py
@@ -0,0 +1,135 @@
+from statistics import mean
+import glob
+import cv2
+import numpy as np
+import utils
+import pytesseract as pt
+
+DEBUGAR = False
+gabarito = ["a", "b", "c", "d", "e", "a", "b", "c", "d", "e"]
+NUMERO_QUESTOES = 10
+NUMERO_ALTERNATIVAS = 5
+
+def corrigir(nome_do_arquivo, gabarito:list = None, numero_questoes=NUMERO_QUESTOES, numero_alternativas=NUMERO_ALTERNATIVAS):
+    img = cv2.imread(nome_do_arquivo)
+    # Redimensiona a imagem para um tamanho padrão 3:4
+    img = cv2.resize(img, (1512, 2016))
+    img_copy = img.copy()
+
+    roi_id = img[0:200, 0:600]  # Ajuste as coordenadas para a região onde o ID está localizado
+    texto_extraido = pt.image_to_string(roi_id, lang='eng')  # Extrai o texto da região
+    id_aluno = None
+
+    # Procurar pelo padrão "ID: x" no texto extraído
+    for linha in texto_extraido.splitlines():
+        if "ID:" in linha:
+            id_aluno = linha.split("ID:")[-1].strip()  # Extrai o valor após "ID:"
+            break
+
+    # Pre Processamentos
+    imagem_sem_sombra = utils.remover_sombra(img)
+    imagem_cinza = cv2.cvtColor(imagem_sem_sombra, cv2.COLOR_BGR2GRAY)
+    imagem_com_desfoque = cv2.GaussianBlur(imagem_cinza, (3, 3), 5)
+    imagem_binaria = cv2.threshold(
+        imagem_com_desfoque, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+    # Encontra os contornos das questões e o retangulo maior
+    contornos, _ = cv2.findContours(
+        imagem_binaria, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    retangulos = utils.encontrar_retangulos(contornos)
+    maior_retangulo = retangulos[0]
+    vertices_maior_retangulo = utils.encontrar_vertices(maior_retangulo)
+    vertices_ordenadas = utils.reordenar_pontos(vertices_maior_retangulo)
+
+    cv2.drawContours(img_copy, vertices_ordenadas, -1, (255, 0, 0), 60)
+    cv2.imshow("img_copy", img_copy)
+
+    # Corrige perspectiva da Imagem
+
+    vertices_float_32 = np.float32(vertices_ordenadas)
+    # pinta a borda de preto para remover-la da área de interesse
+    cv2.drawContours(imagem_binaria, [maior_retangulo], -1, (0, 0, 0), 16)
+
+    template_formato_retangulo = np.float32(
+        [[0, 0], [267*numero_alternativas, 0], [0, 193*numero_questoes], [267*numero_alternativas, 193*numero_questoes]])  # shape conhecido do retangulo em pixels
+    matriz_de_transformacao = cv2.getPerspectiveTransform(
+        vertices_float_32, template_formato_retangulo)
+    img_corrigida = cv2.warpPerspective(
+        imagem_binaria, matriz_de_transformacao, (267*numero_alternativas, 193*numero_questoes))
+    img_bordas_cortadas = utils.cortar_imagem(
+        img_corrigida, 0.99)  # corta 1% das bordas e mantém 99% da imagem
+
+    img_linhas = utils.fatiar_vertical(img_bordas_cortadas, numero_questoes)
+
+    respostas = []
+    pontuacao = 0
+    for indice_linha, linha in enumerate(img_linhas):
+        maior_pixels = 0
+        indice_marcado = -1
+        img_colunas = utils.fatiar_horizontal(linha, numero_alternativas)
+        numero_pixels_na_coluna = []
+        for indice_coluna, coluna in enumerate(img_colunas):
+            coluna = utils.cortar_imagem(coluna, 0.90)
+            numero_de_pixels_brancos = cv2.countNonZero(coluna)
+            numero_pixels_na_coluna.append(numero_de_pixels_brancos)
+            if (DEBUGAR):
+                cv2.imshow("imagem_circulo"+str(indice_linha) +
+                           "_" + str(indice_coluna)+"_"+str(numero_de_pixels_brancos), coluna)
+        numero_pixels_na_coluna_sem_maior = numero_pixels_na_coluna.copy()
+        numero_pixels_na_coluna_sem_maior.remove(max(numero_pixels_na_coluna))
+        media_de_pixels = mean(numero_pixels_na_coluna_sem_maior)
+        for indice_pixels, pixels in enumerate(numero_pixels_na_coluna):
+            if (pixels > maior_pixels and pixels > media_de_pixels*(1.1+0.03*numero_alternativas)):
+                maior_pixels = pixels
+                indice_marcado = indice_pixels
+
+        alternativa_em_letra = utils.ober_alternativa_pelo_indice(
+            indice_marcado)
+        respostas.append(alternativa_em_letra)
+        if (len(gabarito) == numero_questoes and indice_marcado == utils.obter_indice_da_alternativa(gabarito[indice_linha])):
+            pontuacao += 1
+
+    if DEBUGAR:
+        # Imagem binária deve ter o retângulo das questões bem destacado e as opções marcadas também
+        # Nos contornos o retângulo das questões deve ser o maior retângulo destacado da imagem
+        copia_contornos = img.copy()
+        cv2.drawContours(copia_contornos, contornos, -1, (0, 0, 255), 3)
+        cv2.drawContours(copia_contornos, [
+                         maior_retangulo], -1, (0, 255, 0), 20)
+        utils.exibir_imagens([('img', img), ('imagem_sem_sombra', imagem_sem_sombra), (
+            'imagem_com_desfoque', imagem_com_desfoque), ('imagem_binaria', imagem_binaria), ('contornos', copia_contornos), ('img_corrigida', img_corrigida), ('img_bordas_cortadas', img_bordas_cortadas)])
+        print("DEBUG Respostas: ", "Arquivo: ", nome_do_arquivo,
+              respostas, "nota: ", pontuacao, "/ 10")
+        print("FIM Debug \n\n\n")
+        cv2.waitKey(0)
+
+    return respostas, pontuacao, id_aluno
+
+
+# Experiência padrão de gabarito, configurar NUMERO_QUESTOES = 10 e NUMERO_ALTERNATIVAS = 5
+#arquivos_a_serem_corrigidos = glob.glob("gabaritos/*")
+
+# Experiência de diagnóstico com 7 linhas, configurar NUMERO_QUESTOES = 7 e NUMERO_ALTERNATIVAS = 3
+# NUMERO_QUESTOES = 7
+# NUMERO_ALTERNATIVAS = 3
+# arquivos_a_serem_corrigidos = glob.glob("diagnosticos/7*")
+
+# Experiência de diagnóstico com 14 linhas, configurar NUMERO_QUESTOES = 7 e NUMERO_ALTERNATIVAS = 3
+# NUMERO_QUESTOES = 14
+# NUMERO_ALTERNATIVAS = 3
+# arquivos_a_serem_corrigidos = glob.glob("diagnosticos/14*")
+
+
+# NUMERO_QUESTOES = 25
+# NUMERO_ALTERNATIVAS = 3
+# arquivos_a_serem_corrigidos = glob.glob("diagnosticos/25*")
+
+if __name__ == "__main__":
+    arquivo = 'C:/Users/LazimR/Documents/GitHub/MatBloom/MatBloom-API/app/api/services/Lazim.png'
+    respostas, pontuacao, id = corrigir(arquivo)
+    nome_do_estudante = arquivo.replace(
+        "gabaritos/", "").replace(".jpg", "").replace(".png", "").replace(".jpeg", "")
+    print("Estudante: ", nome_do_estudante, "Respostas: ",
+          respostas, "Nota: ", pontuacao, "/ 10")
+    print("ID: ", id)
---
+++ app/api/services/pdf_generate.py
@@ -0,0 +1,151 @@
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.units import inch
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, PageBreak
+from reportlab.lib import colors
+from reportlab.platypus import Table, TableStyle
+from reportlab.lib.enums import TA_LEFT, TA_CENTER
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.units import mm
+from reportlab.pdfgen import canvas
+from reportlab.lib.colors import black
+
+def pdf_test_generate(out_path: str, test_name: str, questions: list, student_name: str, student_id: str = None):
+    """
+    Gera um PDF com a prova para o aluno e um gabarito separado
+    
+    Args:
+        out_path: Caminho para salvar o PDF
+        test_name: Nome da prova
+        questions: Lista de dicionários com as questões
+        student_name: Nome do aluno
+        student_id: ID do aluno
+    """
+    doc = SimpleDocTemplate(out_path, pagesize=letter)
+    styles = getSampleStyleSheet()
+    
+    # Estilos customizados
+    styles.add(ParagraphStyle(
+        name='QuestionTitle',
+        parent=styles['Normal'],
+        fontName='Helvetica-Bold',
+        fontSize=12,
+        spaceAfter=6
+    ))
+    
+    styles.add(ParagraphStyle(
+        name='QuestionText',
+        parent=styles['Normal'],
+        fontName='Helvetica',
+        fontSize=10,
+        leading=14,
+        spaceAfter=12
+    ))
+    
+    styles.add(ParagraphStyle(
+        name='ItemText',
+        parent=styles['Normal'],
+        fontName='Helvetica',
+        fontSize=10,
+        leftIndent=12,
+        spaceAfter=6
+    ))
+    
+    # Conteúdo da prova
+    story = []
+    
+    # Cabeçalho da prova
+    story.append(Paragraph(test_name, styles['Title']))
+    story.append(Paragraph(f"Aluno: {student_name}  ID: {student_id}", styles['Normal']))
+    story.append(Spacer(1, 0.5 * inch))
+    
+    # Adiciona questões
+    for i, question in enumerate(questions, 1):
+        story.append(Paragraph(f"Questão {i}", styles['QuestionTitle']))
+        story.append(Paragraph(question['enunciation'], styles['QuestionText']))
+        
+        # Adiciona itens (alternativas)
+        for j, item in enumerate(question['itens'], 1):
+            story.append(Paragraph(f"{chr(64 + j)}) {item}", styles['ItemText']))
+        
+        story.append(Spacer(1, 0.2 * inch))
+        
+        # Adiciona quebra de página automática a cada 5 questões
+        if i % 5 == 0:
+            story.append(PageBreak())
+
+    # Gera o PDF
+    doc.build(story)
+
+
+
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.units import mm
+from reportlab.pdfgen import canvas
+from reportlab.lib.colors import black
+
+def generate_answer_sheet(out_path: str, student_name: str, student_id: str, num_questions: int = 10, num_itens: int = 5):
+    """
+    Gera um gabarito idêntico ao template fornecido
+    
+    Args:
+        out_path: Caminho para salvar o PDF
+        student_name: Nome do aluno
+        num_questions: Número total de questões (padrão 10)
+    """
+    c = canvas.Canvas(out_path, pagesize=letter)
+    width, height = letter
+    
+    box_width = 40*num_itens  # Largura da caixa
+    box_height = 25.2 * num_questions  # Altura da caixa
+    circle_start_x = 40 * mm  # Posição inicial (margem da borda esquerda)
+    circle_spacing = 15 * mm  # Espaçamento entre os círculos
+    circle_radius = 5  # Raio dos círculos
+    options = [chr(64 + j) for j in range(1, num_itens + 1)]
+    
+    c.setFont("Helvetica-Bold", 14)
+    c.drawString(20*mm, height-20*mm, "Aluno: " + student_name)
+    c.drawString(18.5*mm, height-26*mm," ID: " + student_id)
+    
+    # Opções de resposta (A-E)
+    c.setFont("Helvetica", 12)
+    options_y = height-40*mm
+    for i, option in enumerate(options):
+        c.drawString(35*mm + i*15*mm, options_y, f"{option}")
+    
+    # Números das questões em uma coluna
+    c.setFont("Helvetica", 10)
+    start_y = options_y - 2*mm
+    
+    for i in range(1, num_questions + 1):
+        c.drawString(20*mm, start_y - i*9*mm, f"Q{i}")
+    
+    x = 100  # Posição X do canto inferior esquerdo
+    y = (options_y - box_height) - 20 # Posição Y do canto inferior esquerdo
+
+    c.setLineWidth(2)
+    c.rect(x, y, box_width, box_height, stroke=1, fill=0)
+    c.setLineWidth(1)
+    for j in range(1, num_questions + 1):
+        for i in range(1, num_itens + 1):
+            c.circle(circle_start_x + (i - 1) * circle_spacing, start_y - 9 * mm * j, circle_radius)
+    
+    
+    c.showPage()  # Finaliza a página 2
+    
+    c.save()
+
+if __name__ == '__main__':
+    pdf_test_generate("prova_aluno.pdf", "Prova de Matemática", [
+        {
+            'enunciation': 'Qual é a raiz quadrada de 16?',
+            'itens': ['2', '4', '8', '16', '32'],
+            'correct_item': 1
+        },
+        {
+            'enunciation': 'Qual é o valor de pi?',
+            'itens': ['3.14', '3.15', '3.16', '3.17', '3.18'],
+            'correct_item': 0
+        }
+    ], "Lazaro Claubert Souza Rodrigues Oliveira")
+    generate_answer_sheet("gabarito_aluno.pdf", "Lazaro Claubert Souza Rodrigues Oliveira", '1', 5,4)
